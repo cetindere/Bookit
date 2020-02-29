67f112bf7647c90949201f8725a52a9332f6f7c7
cetindere <58632371+cetindere@users.noreply.github.com>
2020-02-29 15:48:08 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,6 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Cancel reservation");
+        // it is from GitHub
     }
 }
